463beb6f1b4a4f78eac5208a7904152697b13a54
Vivek <vrinumella@gmail.com>
2024-10-05 09:28:27 -0400

    made progress bar show on education

--- cfg-team3/src/pages/education.js
+++ cfg-team3/src/pages/education.js
@@ -9,18 +9,21 @@ export default function Courses() {
       Instructor: "Heart in the Game",
       ModulesLeft: "5",
       NextLesson: "Understanding the Difference Between Heart Attack and Sudden Cardiac Arrest",
+      Progress: 30, // Progress in percentage
     },
     {
       CourseName: "CPR Basics: Your First Line of Defense",
       Instructor: "American Red Cross",
       ModulesLeft: "3",
       NextLesson: "Hands-Only CPR Technique",
+      Progress: 60, // Progress in percentage
     },
     {
       CourseName: "AED Essentials: Using Automated External Defibrillators",
       Instructor: "American Red Cross",
       ModulesLeft: "2",
       NextLesson: "AED Pad Placement",
+      Progress: 75, // Progress in percentage
     },
   ];
 
@@ -28,16 +31,16 @@ export default function Courses() {
     <>
       <Navbar />
       <div className="bg-slate-100">
-        <div className=" max-w-screen-md mx-auto pb-[200px]">
+        <div className="max-w-screen-md mx-auto pb-[200px]">
           <div className="text-3xl font-semibold py-4 text-center">Courses</div>
-          <div className="flex flex-col flex-wrap justify-center ">
-            {CoursesInProgress.map((course, idx) => {
-              return <Course key={idx} course={course} />;
-            })}
+          <div className="flex flex-col flex-wrap justify-center">
+            {CoursesInProgress.map((course, idx) => (
+              <Course key={idx} course={course} />
+            ))}
           </div>
         </div>
       </div>
-      <Footer></Footer>
+      <Footer />
     </>
   );
 }
