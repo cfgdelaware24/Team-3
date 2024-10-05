48923f0526f9cb3bb712d3aaa7e0e9a244c6e268
Luke Tao <lyt5@cornell.edu>
2024-10-05 00:07:21 -0400

    Add route to event registration

--- cfg-team3/src/App.js
+++ cfg-team3/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import About from "./pages/about.js";
+import EventRegistration from "./pages/event-registration.jsx";
+import Events from "./pages/events.jsx";
 import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route path="/" element={<About />} />
+        <Route path="/" element={<Events />} />
+        <Route path="/event-registration/" element={<EventRegistration />} />
       </Routes>
     </HashRouter>
   );
