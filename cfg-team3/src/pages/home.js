017aa0e285d0408bfc4451ad908e19eb8ce6a749
woverfield <wkoverfield@gmail.com>
2024-10-04 20:28:37 -0500

    Featured stories on homepage

--- cfg-team3/src/pages/home.js
+++ cfg-team3/src/pages/home.js
@@ -8,8 +8,8 @@ export default function home() {
   return (
     <>
       <Navbar />
-      <div className="bg-black w-full">
-        <div className="flex container mx-auto items-center w-full text-white p-5 gap-10 px-28">
+      <div className="bg-black w-full section">
+        <div className="flex container mx-auto items-center w-full text-white p-20 gap-10 px-28">
           <div className="hero-left flex flex-col gap-5">
             <p className="text-2xl">
               Sudden Cardiac Arrest is the #1 cause of death for student
@@ -26,7 +26,7 @@ export default function home() {
           </div>
         </div>
       </div>
-      <div className="text-black w-full">
+      <div className="text-black w-full section">
         <div className="flex container mx-auto items-center p-5 px-28 gap-10">
           <div className="hero-left my-10">
             <img src={SchoolImg} alt="" />
@@ -40,10 +40,44 @@ export default function home() {
               the importance and simplicity of bystander CPR.
             </p>
             <div className="button-container flex justify-between gap-5 text-xl">
-              <HomeBtn text="More Info" isTransparent={false} />
-              <HomeBtn text="Donate" isTransparent={true} />
+              <button className="bg-red-400 p-5 rounded-md w-full">
+                More Info
+              </button>
+              <button className="bg-transparent border-2 border-red-400 p-5 rounded-md w-full">
+                Volunteer Signup
+              </button>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="text-black w-full section">
+        <div className="flex justify-center items-center gap-20">
+          <div className="flex flex-col items-center">
+            <div className="bg-slate-400 rounded-full h-48 w-48 flex items-center justify-center"></div>
+            <p className="mt-2 text-center max-w-xs">
+              A free non evasive EKG took less than 15 minutes and kept Zach
+              from possibly being a statistic
+            </p>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="bg-slate-400 rounded-full h-48 w-48 flex items-center justify-center"></div>
+            <p className="mt-2 text-center max-w-xs">
+              A free non evasive EKG took less than 15 minutes and kept Zach
+              from possibly being a statistic
+            </p>
+          </div>
+          <div className="flex flex-col items-center">
+            <div className="bg-slate-400 rounded-full h-48 w-48 flex items-center justify-center"></div>
+            <p className="mt-2 text-center max-w-xs">
+              A free non evasive EKG took less than 15 minutes and kept Zach
+              from possibly being a statistic
+            </p>
+          </div>
+        </div>
+        <div className="button-container flex justify-center my-10">
+          <button className="bg-red-400 p-5 rounded-md">
+            View More Stories
+          </button>
         </div>
       </div>
     </>
