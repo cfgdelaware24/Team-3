747f344d66570c2d407f08f815cfd90666b41396
Hamza <hamzachaudhrycs@gmail.com>
2024-10-05 06:19:53 -0400

    svg

--- cfg-team3/src/pages/profile.js
+++ cfg-team3/src/pages/profile.js
@@ -1,5 +1,6 @@
 import Event from "../components/event";
 import Navbar from "../components/navbar";
+import MyIcon from "../images/usericon.svg";
 
 export default function EventDetail() {
   const RegisteredEvents = [
@@ -24,17 +25,34 @@ export default function EventDetail() {
       volunteersCount: 5,
       ekg: 5,
     },
+    {
+      id: "asdf",
+      name: "Deleware State High School",
+      location: "2813 W 17th St, Wilmington, DE 19806",
+      sponsor: "Team 3",
+      dateTime: "11/14/2024 5:00PM",
+      volunteersCount: 5,
+      ekg: 5,
+    },
   ];
 
   return (
     <>
       <Navbar />
-      <div className="bg-slate-100">
-        <div className="max-w-screen-md mx-auto pb-[200px]">
-          <div className="text-3xl font-semibold py-4">
-            Hi John Doe! Here are your events:
-          </div>
-          <div className="flex flex-col flex-wrap justify-center">
+      <div className="ml-[200px]  flex justify-between items-start px-10">
+        {/* Icon section */}
+        <div className="flex-shrink-0 mt-20">
+          <img
+            src={MyIcon}
+            alt="My SVG Icon"
+            style={{ width: "200px", height: "200px" }} // Adjust the size as necessary
+          />
+          <p className="ml-[55px] text-2xl">John Doe</p>
+        </div>
+
+        {/* Event cards section */}
+        <div className="max-w-screen-md  flex-grow ml-10">
+          <div className="flex flex-col flex-wrap justify-center pt-10">
             {RegisteredEvents.map((event, idx) => {
               return <Event key={idx} event={event} status="registered" />;
             })}
