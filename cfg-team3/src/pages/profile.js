b210cd1974d9449e0105c17666d295aaeb6cd641
Hamza <hamzachaudhrycs@gmail.com>
2024-10-05 07:42:18 -0400

    icon fix

--- cfg-team3/src/pages/profile.js
+++ cfg-team3/src/pages/profile.js
@@ -4,6 +4,7 @@ import Footer from "../components/footer";
 import Navbar from "../components/navbar";
 import { firestore } from "../firebase"; // Import Firestore instance
 import { collection, getDocs } from "@firebase/firestore";
+import MyIcon from "../images/usericon.svg"
 
 export default function EventDetail() {
   const userId = "SjnfUeRrj3MAMfkOIYPX";
