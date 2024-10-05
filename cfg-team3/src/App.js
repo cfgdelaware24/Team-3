5afca443bbf613ad8c4725d6935c75aba02cde64
Hamza <hamzachaudhrycs@gmail.com>
2024-10-04 20:41:45 -0400

    Routing For App

--- cfg-team3/src/App.js
+++ cfg-team3/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import About from "./pages/about.js";
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <div></div>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<About />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
